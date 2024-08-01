1d1dba1ed020f2b0cb7cbc3f7e1ebabef8835a74
Ravana2130 <164500270+Ravana2130@users.noreply.github.com>
2024-08-01 14:34:11 +0530

    Add files via upload

---
+++ day1/script.js
@@ -0,0 +1,17 @@
+// let age=25
+// if (age>=18){
+//     console.log("You can Vote")
+// }else{
+//     console.log("You can Not Vote")
+// }
+
+// task 2
+// let num=20
+// if (num %2===0){
+//     console.log(num,"is even")
+// } else{
+//     console.log(num,"is odd")
+// }/
+
+
+// task3
